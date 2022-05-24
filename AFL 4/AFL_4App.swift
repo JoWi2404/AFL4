28ff10c78ba11df2e97dc2e38a2fe057c552c0df
JoWi2404 <joeywiryawan2@gmail.com>
2022-05-24 09:58:49 +0700

    Add new Files

---
+++ AFL 4/AFL_4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL_4App.swift
+//  AFL 4
+//
+//  Created by Macbook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
